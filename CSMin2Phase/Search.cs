c9cf14ef71f59b173fefdffea14e28fc1dd01ae7
CuberMessenger <cuber_messenger@hotmail.com>
2018-03-12 14:43:38 +0800

    starting to translate

---
+++ CSMin2Phase/Search.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSMin2Phase {
+    class Search {
+        public static bool USE_TWIST_FLIP_PRUN = true;
+    }
+}
