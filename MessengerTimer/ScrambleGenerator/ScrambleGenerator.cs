e62a51cc2bf65333ee12359ccd6edc7810946575
CuberMessenger <cuber_messenger@hotmail.com>
2018-08-13 18:24:45 +0800

    importing TNoodle>NET and fixing MainPage UI

--- MessengerTimer/ScrambleGenerator/ScrambleGenerator.cs
+++ MessengerTimer/ScrambleGenerator/ScrambleGenerator.cs
@@ -3,40 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CSMinTwoPhase;
+using TNoodle.Puzzles;
+using static TNoodle.Puzzles.CubePuzzle;
 
 namespace MessengerTimer {
     public enum ScrambleType { x3, x2, x4, x5, x6, x7 }
     internal class ScrambleGenerator {
         internal static int ScrambleOrder => int.Parse(ScrambleType.ToString().Substring(1));
         internal static ScrambleType ScrambleType { get; set; }
+        private static Random Rand { get; set; }
+
+        static ScrambleGenerator() {
+            Rand = new Random();
+        }
+
         internal static (string, string) Generate() {
-            switch (ScrambleType) {
-                case ScrambleType.x3:
-                    return Generate3x3();
-                case ScrambleType.x2:
-                    break;
-                case ScrambleType.x4:
-                    break;
-                case ScrambleType.x5:
-                    break;
-                case ScrambleType.x6:
-                    break;
-                case ScrambleType.x7:
-                    break;
-                default:
-                    break;
+            if ((int)ScrambleType <= 5) {
+                return GenerateCubePuzzle();
             }
+
             return ("", "");
         }
 
-        //private static (string, string) Generate5x5() {
-
-        //}
-
-        private static (string, string) Generate3x3() {
-            string cube = Tools.randomCube();
-            string scramble = new Search().solution(cube, 21, 1000000, 0, Search.INVERSE_SOLUTION);
+        private static (string, string) GenerateCubePuzzle() {
+            var puzle = new CubePuzzle(ScrambleOrder);
+            string scramble = puzle.GenerateWcaScramble(Rand);
+            string cube = ((new CubeState(puzle)).ApplyAlgorithm(scramble) as CubeState).ToFaceCube();
             return (cube, scramble);
         }
     }
